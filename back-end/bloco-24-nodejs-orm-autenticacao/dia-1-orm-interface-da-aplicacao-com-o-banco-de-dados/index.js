98145a18e4024821d6073fa2c3af013f3c52c662
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-05-31 18:42:52 -0300

    feat: exercicios 1 e 2

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', async (_req, res) => {
-  return res.status(200).send();
+  try {
+    return res.status(200).json({ message: 'The API is working fine' });
+  } catch (err) {
+    console.log('Erro ao carregar a página: ', err.message);
+    return res.status(400).json({ message: 'Internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
