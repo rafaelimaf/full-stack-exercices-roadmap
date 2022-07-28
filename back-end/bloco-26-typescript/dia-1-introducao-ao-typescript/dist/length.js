3b8d89113d75c5dc8b791a12281f62d54327af33
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-07-28 16:57:09 -0300

    refact: correção do nome de algumas funções para ficarem mais semânticas

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/dist/length.js
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/dist/length.js
@@ -1,9 +1,9 @@
 "use strict";
-const units = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
-function convert(value, baseUnit, finalUnit) {
-    const baseIndex = units.indexOf(baseUnit); // pegamos o index da unidade base no array
-    const finalIndex = units.indexOf(finalUnit); // pegamos o index da unidade para a conversão
+const lengthUnits = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+function convertLength(value, baseUnit, finalUnit) {
+    const baseIndex = lengthUnits.indexOf(baseUnit); // pegamos o index da unidade base no array
+    const finalIndex = lengthUnits.indexOf(finalUnit); // pegamos o index da unidade para a conversão
     const exponent = (baseIndex - finalIndex); // calculamos o expoente a partir da diferença dos index
     return value * Math.pow(10, exponent);
 }
-console.log(convert(10, 'km', 'dam'));
+console.log(convertLength(10, 'km', 'dam'));
--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/src/length.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/src/length.ts
@@ -1,11 +1,11 @@
-const units = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+const lengthUnits = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 
-function convert (value: number, baseUnit: string, finalUnit: string): number {
-  const baseIndex = units.indexOf(baseUnit); // pegamos o index da unidade base no array
-  const finalIndex = units.indexOf(finalUnit); // pegamos o index da unidade para a conversão
+function convertLength (value: number, baseUnit: string, finalUnit: string): number {
+  const baseIndex = lengthUnits.indexOf(baseUnit); // pegamos o index da unidade base no array
+  const finalIndex = lengthUnits.indexOf(finalUnit); // pegamos o index da unidade para a conversão
   const exponent = (baseIndex - finalIndex); // calculamos o expoente a partir da diferença dos index
 
   return value * Math.pow(10, exponent);
 }
 
-console.log(convert(10, 'km', 'dam'));
+console.log(convertLength(10, 'km', 'dam'));
